9167b6c36adcf9a6171c3ac71d01c1d3421fc633
Haben Habtom <hh@netinventors.de>
2024-05-29 23:48:47 +0200

    Your commit message

---
+++ src/views/Catalog.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="product-list">
+    <div class="product"
+    v-for="product in store.products"
+    :key="product.id"
+    @click="goToProductPage(product.id)"
+    >
+<img :src="product.thumbnail" alt="">
+      <h2>Brand: {{ product.brand }}</h2>
+      <p>Description: {{ product.description }}</p>
+      <p>Price: ${{ product.price }}</p>
+
+
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+
+import { defineComponent }  from "vue";
+
+export default defineComponent({
+  name: 'CatalogVue'
+})
+
+</script>
+
+<script setup>
+
+import { onMounted } from "vue";
+import { productsStore } from "@/stores/products"
+import { useRouter } from "vue-router";
+
+const  store = productsStore()
+const router = useRouter()
+
+const goToProductPage = (id) => {
+  router.push({ name: 'ProductView', params: { id } })
+}
+
+
+
+onMounted(async () =>  {
+  await store.fetchProductsFromDB()
+  console.log('MOUNTED >>>>>>>', store.products)
+
+})
+
+
+</script>
+
+
+
+<style>
+
+.product-list {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.product {
+
+  flex-basis: 28%;
+  margin: 8px;
+  padding: 16px;
+  box-shadow: 0px 0px 14px 1px #e6e6;
+  }
+
+  .product img {
+    width: 70%
+  }
+
+
+</style>
